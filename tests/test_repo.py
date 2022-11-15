f54d79327807b7ff0bd8b03a925aef13de15dd67
Chen Yang <yangchen@thinkmore.info>
2022-11-15 10:44:54 -0500

    test dirty

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -19,6 +19,13 @@ def tmp_repo():
     repo = Repo(repo_path)
     return repo
 
+@pytest.fixture
+def tmp_files():
+    tmp_path = Path("./tests/fake_datas/case1/tmp_files")
+    shutil.rmtree(tmp_path, ignore_errors=True)
+    tmp_path.mkdir(0o755)
+    return tmp_path
+
     
 
 def test_same_file(tmp_repo):
@@ -26,3 +33,14 @@ def test_same_file(tmp_repo):
     assert tmp_repo.query(file_name) == QueryResult.NEW
     tmp_repo.file_helper(file_name).create_infos()
     assert tmp_repo.query(file_name) == QueryResult.SAME
+
+
+def test_dirty_file(tmp_files, tmp_repo):
+    path = tmp_files / "dirty_file"
+    with path.open("w") as fd:
+        fd.write("hello1")
+    tmp_repo.file_helper(path).create_infos()
+    with path.open("w") as fd:
+        fd.write("hello2")
+    assert tmp_repo.query(path) == QueryResult.DIRTY
+
