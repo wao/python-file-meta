7cc5cabdea44418e6ddf910c1e70b8ab29fcf9b5
Chen Yang <yangchen@thinkmore.info>
2022-11-15 10:36:11 -0500

    test same

--- file_meta/repo.py
+++ file_meta/repo.py
@@ -4,6 +4,8 @@ from loguru import logger
 import yaml
 import hashlib
 from enum import Enum
+from dataclasses import dataclass, asdict
+from datetime import datetime
 
 class Md5sum:
     def __init__(self, md5sum : str):
@@ -26,19 +28,27 @@ class Md5sum:
     def __eq__(self, value)->bool:
         return self.md5sum == value.md5sum
 
+@dataclass
+class PathHistory:
+    time : datetime
+    path : Path
+
+
+@dataclass
 class ObjectInfo:
-    def __init__(self, md5sum : Md5sum, paths ):
-        self.md5sum = md5sum
-        self.paths = []
-        self.comment = "" 
-        self.parent = None
+    md5sum : Md5sum
+    st_size : int
+    paths : list[PathHistory]
+    comment : str
 
-
+@dataclass
 class StagingInfo:
-    def __init__(self, path : Path):
-        self.path = path
-        self.md5sum = Md5sum.from_file(path)
-        self.track = False
+    md5sum : Md5sum
+    st_dev : int
+    st_size : int
+    st_mtime : float
+    st_ctime : float
+
 
 class RepoFileHelper:
     def __init__(self, file_path : Path, root_path : Path ):
@@ -47,6 +57,8 @@ class RepoFileHelper:
         self.root = root_path
         self._file_path = None
         self._object_path = None
+        self._staging_info = None
+        self._object_info = None
 
     @staticmethod
     def from_file( file_path, root_path ):
@@ -74,27 +86,61 @@ class RepoFileHelper:
     def has_object_info(self):
         return self.object_path.exists()
 
+    @property
     def staging_info(self) -> StagingInfo:
         assert self.has_staging_info()
 
         if not self._staging_info:
             with self.staging_path.open("r") as fd:
-                self._staging_info = yaml.safe_load(fd) 
+                self._staging_info = StagingInfo(**yaml.unsafe_load(fd)) 
 
         return self._staging_info
 
+    @property
     def object_info(self):
         assert self.has_object_info()
 
         if not self._object_info:
             with self.object_path.open("r") as fd:
-                self._object_info = yaml.safe_load(fd) 
+                self._object_info = ObjectInfo(**yaml.unsafe_load(fd)) 
 
         return self._object_info
 
-    def create_object_info(self):
+    def create_infos(self):
         assert not self.has_object_info()
-        return None
+        assert not self.has_staging_info()
+
+        current_stat = self._src_file_path.stat()
+
+        self._staging_info = StagingInfo( self.md5sum, 
+                    current_stat.st_dev, current_stat.st_size, current_stat.st_mtime, current_stat.st_ctime) 
+
+        self._object_info = ObjectInfo(self.md5sum, current_stat.st_size, [ 
+                PathHistory( datetime.now(), self._src_file_path.absolute() ),
+            ], "")
+
+        with self.staging_path.open("w") as fd:
+            yaml.dump( asdict(self._staging_info), fd)
+
+        with self.object_path.open("w") as fd:
+            yaml.dump( asdict(self._object_info), fd)
+
+
+    def query(self):
+        #if find same file_name in repo
+        if self.has_staging_info():
+            #if same md5sum
+            if self.staging_info.md5sum == self.md5sum:
+                return QueryResult.SAME
+            else:
+                return QueryResult.DIRTY
+        else:
+            #find same md5sum in repo
+            if self.has_object_info():
+                return QueryResult.NEW_NAME
+            else:
+                #create a new entry and save all items
+                return QueryResult.NEW
 
 
 class QueryResult(Enum):
@@ -108,22 +154,9 @@ class Repo:
         self.root = root_path
 
     def query(self, file_name : Path ):
-        #caculate md5sum of file_name
-        filehelper = RepoFileHelper.from_file(file_name, self.root)
+        return self.file_helper(file_name).query()
 
-        #if find same file_name in repo
-        if filehelper.has_staging_info():
-            #if same md5sum
-            if filehelper.staging_info.md5sum == filehelper.md5sum:
-                return QueryResult.SAME
-            else:
-                return QueryResult.DIRTY
-        else:
-            #find same md5sum in repo
-            if filehelper.has_object_info():
-                return QueryResult.NEW_NAME
-            else:
-                #create a new entry and save all items
-                return QueryResult.NEW
             
+    def file_helper(self, file_name : Path) :
+        return RepoFileHelper.from_file(file_name, self.root)
         
--- tests/test_repo.py
+++ tests/test_repo.py
@@ -1,6 +1,7 @@
 from file_meta.repo import Repo, QueryResult
 from pathlib import Path
 import pytest
+import shutil
 
 @pytest.fixture
 def case1_repo():
@@ -10,5 +11,18 @@ def case1_repo():
 def test_new_file(case1_repo):
     assert case1_repo.query(Path("./tests/fake_datas/case1/files/README.rst")) == QueryResult.NEW 
 
-def test_same_file(case1_repo):
-    assert case1_repo.query(Path("./tests/fake_datas/case1/files/same_file")) == QueryResult.SAME
+@pytest.fixture
+def tmp_repo():
+    repo_path = Path("./tests/fake_datas/case1/tmp_repo")
+    shutil.rmtree(repo_path, ignore_errors=True)
+    repo_path.mkdir(0o755)
+    repo = Repo(repo_path)
+    return repo
+
+    
+
+def test_same_file(tmp_repo):
+    file_name = Path("./tests/fake_datas/case1/files/same_file")
+    assert tmp_repo.query(file_name) == QueryResult.NEW
+    tmp_repo.file_helper(file_name).create_infos()
+    assert tmp_repo.query(file_name) == QueryResult.SAME
